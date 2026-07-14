7b039bd1d7f46f16f98f8967c13f6803acc7e1c6
Another945 <112581804+Another945@users.noreply.github.com>
2026-07-14 11:50:46 -0500

    modified: server.js

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ app.use(cors());
 app.use(express.json());
 
 const conexion = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 });
 
 conexion.connect((error) => {
